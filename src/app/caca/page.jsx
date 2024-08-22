76c7537565d0fe7638e1981d518f607a347f031a
LifeofLion <133486557+LifeofLion@users.noreply.github.com>
2024-08-22 12:00:42 +0200

    initial commit

---
+++ src/app/caca/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '@/components/Button'
+
+export default function page() {
+  return (
+    <div>
+        <div>caca</div>
+        <Button />
+    </div>
+  )
+}
